e659993a2c8918db06f267e5db5bcc1fc158f6ba
Nishchay Bhatt <bhatt.nishchay@royalbrothers.com>
2023-09-03 16:51:33 +0530

    Changed Readme

--- Readme.md
+++ Readme.md
@@ -2,3 +2,11 @@
 
 
 This is a simple repo to display my JS understanding.
+# sum.js
+Page is a sum of 1-100 integer
+
+# pattern.js
+Page shows some patterns
+
+# fibonacci.js
+Page shows fibbonacci series till first 100 digits
--- pattern.js
+++ pattern.js
@@ -1,4 +1,4 @@
-function pattern(length) {
+function pattern1(length) {
   for (var i = 0; i < length; i++) {
     var str = "";
     for (j = 1; j <= i + 1; j++) {
@@ -8,4 +8,21 @@ function pattern(length) {
   }
 }
 
-pattern(10);
+function pattern2(length) {
+  for (var i = 0; i < length; i++) {
+    var str = "";
+    for (var j = 1; j < length - i; j = j + 1) {
+      str += "  ";
+    }
+    for (var j = 1; j <= i + 1; j++) {
+      str += "* ";
+    }
+    for (var j = 1; j < length - i; j = j + 1) {
+      str += "  ";
+    }
+    console.log(str);
+  }
+}
+
+pattern1(10);
+pattern2(10);
